94f9c8f17a35480692cea3e4e76e9e1a7f130c7b
Kassandra <K1995Moreno@gmail.com>
2022-11-05 14:25:10 -0400

    commit tree 262581b2dfb1f4a65bc527ff979418d4f55c25cf

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const element= document.createElement('div')
+document.body.append(element)
+const ul= document.createElement('ul')
+
+for (let i; i < 3; i++) {
+    const li= document.createElement('li')
+    li.innerText= (i + 1).toString()
+    ul.append(li)
+}
+
+element.append(ul)
